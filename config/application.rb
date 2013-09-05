158f38eb112137557d4534408efe7e0b6d85d3b2
mscoutermarsh <coutermarsh.mike@gmail.com>
2013-09-04 21:01:23 -0400

    actionmailer settings

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,14 @@ Bundler.require(:default, Rails.env)
 
 module Scrumlogs
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.action_mailer.smtp_settings = {
+        :address   => "smtp.mandrillapp.com",
+        :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+        :enable_starttls_auto => true, # detects and uses STARTTLS
+        :user_name => configatron.emails.mandrill.user,
+        :password  => configatron.emails.mandrill.pass, # SMTP password is any valid API key
+        :authentication => 'login', # Mandrill supports 'plain' or 'login'
+        :domain => configatron.emails.mandrill.domain, # your domain to identify your server when connecting
+    }
   end
 end
--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -1,6 +1,11 @@
 # Email
 configatron.emails.from = 'goodmorning@scrumlogs.com'
 
+configatron.emails.mandrill.domain = 'scrumlogs.com'
+configatron.emails.mandrill.user = ENV['MANDRILL_USER']
+configatron.emails.mandrill.pass = ENV['MANDRILL_PASS]
+
+
 # Integrations
 configatron.github.client_id = '12345'
 configatron.github.secret = ENV['GITHUB_SECRET']
