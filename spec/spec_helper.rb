f130f43b5407ea35a8fa65f2e28f33cfc78abfed
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2013-11-03 15:07:40 -0500

    fix deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,8 +43,6 @@ Spork.prefork do
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
 
-    config.extend VCR::RSpec::Macros
-
     # Run specs in random order to surface order dependencies. If you find an
     # order dependency and want to debug it, you can fix the order by providing
     # the seed, which is printed after each run.
