9aed471830a3a2f78adf6742ae304344ea9b11cc
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2013-11-03 19:29:42 -0500

    data collector spec

--- spec/workers/data_collector_spec.rb
+++ spec/workers/data_collector_spec.rb
@@ -6,5 +6,17 @@ describe DataCollector do
   it { should be_retryable 1 }
 
   let(:data_collector){ DataCollector.new }
+
+  describe '#collect_events' do
+    it 'raises error if not overridden' do
+      expect{ data_collector.collect_events }.to raise_error
+    end
+  end
+
+  describe '#integration_name' do
+    it 'raises error if not overridden' do
+      expect{ data_collector.integration_name }.to raise_error
+    end
+  end
   
 end
