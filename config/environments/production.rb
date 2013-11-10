f56a500e6d1df3f80a56425b9a15fc35b86a06f7
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2013-11-10 14:13:19 -0500

    asset expire

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ Scrumlogs::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+  config.static_cache_control = "public, max-age=31536000"
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
