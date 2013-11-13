06b2f5ff84dae66bb5cef824e8efee9cba264928
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2013-11-13 07:11:32 -0500

    production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Scrumlogs::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'scrumlogs.com' }
+
 end
