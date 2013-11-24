e5395dc5b6fd06ce7d7f008671b0aa8911376102
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2013-11-24 13:28:16 -0500

    config redis as cache store. fix travis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,12 @@ Scrumlogs::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.cache_store = :redis_store, "#{ENV["REDISTOGO_URL"]}/1"
+  config.action_dispatch.rack_cache = {
+      metastore:   "#{ENV["REDISTOGO_URL"]}/1/metastore",
+      entitystore: "#{ENV["REDISTOGO_URL"]}/1/entitystore"
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
